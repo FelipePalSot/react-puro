505b98ee6e56c1b9368b3bcc4cac6f45c27a7061
Felipe Jean Franco Palomino Sotelo <felipejeanfrancopalominosotelo@MacBook-Pro-de-Felipe.local>
2025-12-25 18:10:21 -0500

    added interface and dynamic props

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
-
-export const ItemCounter = () => {
-  return (
-    <div>ItemCounter</div>
-  )
+interface Props {
+    name: string;
+    quantity?: number;
 }
+
+export const ItemCounter = ({name, quantity}: Props) => {
+    
+  return (
+    <section
+    style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 10,
+        marginTop: 10,
+        }}
+    >
+        <span
+        style={{
+            width: 150,
+        }}
+        >
+            {name}
+        </span>
+        <button>+1</button>
+        <span>{quantity}</span>
+        <button>-1</button>
+    </section>
+  );
+  
+};

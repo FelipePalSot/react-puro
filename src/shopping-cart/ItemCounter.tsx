85534a5e73a7c9d3738f35f6a63aed59e74ed6f7
Felipe Jean Franco Palomino Sotelo <felipejeanfrancopalominosotelo@MacBook-Pro-de-Felipe.local>
2025-12-25 22:10:00 -0500

    added css files for react

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,29 +1,55 @@
+
+import { useState } from "react";
+
+// import './ItemCounter.css';
+import styles from './ItemCounter.module.css';
+
 interface Props {
     name: string;
     quantity?: number;
 }
 
-export const ItemCounter = ({name, quantity}: Props) => {
+export const ItemCounter = ({name, quantity = 1}: Props) => {/**Puse quantity = 1, porque en el interface quantity? , es opcional  */
+
+    const [count, setCount] = useState(quantity); /**USando useState */
+
+    // const handleClick = (event: MouseEvent) =>{
+    //     console.log(`Click en ${name}`)
+    // }
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+
+    const handleSubtract = () => {
+        if( count === 1) return;
+
+        setCount(count - 1);
+    }
     
   return (
     <section
-    style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 10,
-        marginTop: 10,
-        }}
+        className={styles.itemRow}
+        // style={{
+        //     display: 'flex',
+        //     alignItems: 'center',
+        //     gap: 10,
+        //     marginTop: 10,
+        //     }}
     >
         <span
-        style={{
-            width: 150,
-        }}
+            className={styles['item-text']}
+            style={{
+                color: count === 1 ? 'red' : 'black' , //si ponia quantity siempre se queda en red, ya que quantity siempre sera 1, count es lo q varia
+            }}
         >
             {name}
         </span>
-        <button>+1</button>
-        <span>{quantity}</span>
-        <button>-1</button>
+        <button onClick={handleAdd}>
+            +1
+        </button>
+        <span>{count}</span>
+        <button onClick={handleSubtract}>-1</button>
     </section>
   );
   
